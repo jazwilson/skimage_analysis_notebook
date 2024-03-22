b5cde0cec5bc269a3be910e8973587c5e3403e49
JazWilson <jjwilson206@gmail.com>
2024-03-21 22:50:55 -0700

    setup and notebook progress

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # skimage_notebook
 Exploring use of skimage for 3D image analysis 
+
+Using + modifying training and lecture material from [scikit-image/skimage-tutorials]('https://github.com/scikit-image/skimage-tutorials/tree/main)
+
+- supplementary_code.py from workshops/2022-data-umbrella
---
+++ check_requirements_txt_setup.py
@@ -0,0 +1,47 @@
+"""Check if runtime dependencies are installed.
+
+The script currently does not check for specified optional requirements, e.g.
+'scikit-image[data]'.
+"""
+
+import sys
+import os
+from packaging.version import parse
+
+if sys.version_info.major < 3:
+    print('[!] You are running an old version of Python, upgrade to python v3. ')
+
+    sys.exit(1)
+
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
+    reqs = f.readlines()
+
+reqs = [(pkg, ver) for (pkg, _, ver) in
+        (req.split() for req in reqs if req.strip())]
+
+pkg_names = {
+    'scikit-image[data]': 'skimage',
+    'scikit-image': 'skimage',
+    'scikit-learn': 'sklearn'
+}
+
+for (pkg, version_wanted) in reqs:
+    module_name = pkg_names.get(pkg, pkg)
+    try:
+        m = __import__(module_name)
+        version_installed = m.__version__
+        if parse(version_wanted) > parse(version_installed):
+            status = 'X'
+        else:
+            status = '✓'
+    except ImportError as e:
+        m = None
+        if (pkg != 'numpy' and 'numpy' in str(e)):
+            status = '?'
+            version_installed = 'Needs NumPy'
+        else:
+            version_installed = 'Not installed'
+            status = 'X'
+    print(
+        '[{}] {:<20} {}'.format(status, pkg.ljust(13), version_installed)
+    )
